36bd7d919517d0c49db1ec310f29b7520503d138
nbianco3 <nick.a.bianco@gmail.com>
2015-06-05 16:25:29 -0400

    Version 1.3

--- NeuroDinnerPresentation.m
+++ NeuroDinnerPresentation.m
@@ -1,7 +1,7 @@
 function NeuroDinnerPresentation(comPort,captureDuration)
 
 numPlotSamples = 500;
-DELAY = 0.2;
+buffer = 0.1;
 
 shimmer1 = ShimmerHandleClass(comPort);
 macs = SetEnabledSensorsMacrosClass;
@@ -15,12 +15,12 @@ if (shimmer1.connect)
     shimmer1.setaccelrange(0);          % +/- 2.0 g
     shimmer1.setgyrorange(1);           % +/- 500 deg/s  
     shimmer1.setmagrange(1);            % +/- 1.0 Ga
-    shimmer1.setgyrorate(45);           % 173.91 Hz (8-bit input --> 255 to 0)
+    shimmer1.setgyrorate(155);          % 51.2 Hz (8-bit input --> 255 to 0)
     shimmer1.setexgrate(3,1);           % 1000 Hz (4 = 2000 Hz)
     shimmer1.setexgrate(3,2);
     shimmer1.setaccelrate(5);           % 100 Hz
     shimmer1.setmagrate(6);             % 75 Hz
-    shimmer1.setbaudrate(10);           % 921600 (max)
+    shimmer1.setbaudrate(10);           % 921600 kB/s (max)
     disp('WARNING: Sensor ranges must match calibrated ranges')
     disp('Press <enter> to continue...')
     pause
@@ -28,18 +28,16 @@ if (shimmer1.connect)
     if (shimmer1.start)
         
         calDataShimmer1 = [];
-        h.figure1=figure('Name','Shimmer 1');
+        h.figure1=figure('Name','Shimmer 1','units','normalized','position',[0 0 1 1]);
         elapsedTime = 0;
         tic;
         
         while (elapsedTime < captureDuration)
             
-            pause(DELAY)
-            packetRateShimmer1 = shimmer1.getpercentageofpacketsreceived();
+            pause(buffer)
         
             % Read and plot data for shimmer1
-            [calData,signalName,signalFormat,signalUnit]=shimmer1.getdata('u');
-            keyboard
+            [calData,signalName,signalFormat,signalUnit]=shimmer1.getdata('c');
             calDataShimmer1 = [calDataShimmer1; calData];    % Read the calibrated data for shimmer1 and add to previous data
             
             if (length(calDataShimmer1)>numPlotSamples)
@@ -57,73 +55,71 @@ if (shimmer1.connect)
                 magIndex(1) = find(ismember(signalName, 'Magnetometer X'));
                 magIndex(2) = find(ismember(signalName, 'Magnetometer Y'));
                 magIndex(3) = find(ismember(signalName, 'Magnetometer Z'));
-                exgIndex(1) = find(ismember(signalName, 'EXG1 CH1'));
-                exgIndex(2) = find(ismember(signalName, 'EXG1 CH2'));
+                emgIndex(1) = find(ismember(signalName, 'EMG CH1'));
+                emgIndex(2) = find(ismember(signalName, 'EMG CH2'));
                 
-                timeDataShimmer = calDataShimmer1(:,timeIndex);
-                packetsReceivedShimmer1 = shimmer1.getpercentageofpacketsreceived(timeDataShimmer);
+                timeDataShimmer1 = calDataShimmer1(:,timeIndex)/1000; % seconds
+                packetsReceivedShimmer1 = shimmer1.getpercentageofpacketsreceived(timeDataShimmer1);
                 accelDataShimmer1 = [calDataShimmer1(:,accelIndex(1)), calDataShimmer1(:,accelIndex(2)), calDataShimmer1(:,accelIndex(3))];
                 gyroDataShimmer1 = [calDataShimmer1(:,gyroIndex(1)), calDataShimmer1(:,gyroIndex(2)), calDataShimmer1(:,gyroIndex(3))];
                 magDataShimmer1 = [calDataShimmer1(:,magIndex(1)), calDataShimmer1(:,magIndex(2)), calDataShimmer1(:,magIndex(3))];
-                exgDataShimmer1 = [calDataShimmer1(:,exgIndex(1)), calDataShimmer1(:,exgIndex(2))];
+                emgDataShimmer1 = [calDataShimmer1(:,emgIndex(1)), calDataShimmer1(:,emgIndex(2))];
+                emgSampleTime = linspace(0,numPlotSamples/1000,numPlotSamples)';
+                emgFiltDataShimmer1 = [processEMG(emgDataShimmer1(:,1),emgSampleTime,[10 400],6), processEMG(emgDataShimmer1(:,1),emgSampleTime,[10 400],6)];
                 
                 set(0,'CurrentFigure',h.figure1);
                 
                 % Plot accelerometer data
-                subplot(4,1,1)
-                plot(accelDataShimmer1);                                       
-                title(['Shimmer 1 Data - ' sprintf('Packets Received: %3.2f %',packetsReceivedShimmer1)]);                         
-                axis([0 NO_SAMPLES_IN_PLOT -20 20]);                           
+                subplot(2,2,1)
+                plot(accelDataShimmer1);                                                             
+                axis([0 numPlotSamples -20 20]);
+                ylabel('Acceleration (m/s^2)')
+                title([sprintf('Samp Rate: %4.1f',100) ' Hz'])
                 legend(char(signalName{accelIndex(1)}),char(signalName{accelIndex(2)}),char(signalName{accelIndex(3)}))     
                 
                 % Plot gyroscope data
-                subplot(4,1,2)
+                subplot(2,2,2)
                 plot(gyroDataShimmer1);                                                             
-                axis([0 NO_SAMPLES_IN_PLOT -500 500]);                           
+                axis([0 numPlotSamples -500 500]);
+                ylabel('Angular Velocity (deg/s)')
+                title([sprintf('Samp Rate: %4.1f',51.2) ' Hz'])
                 legend(char(signalName{gyroIndex(1)}),char(signalName{gyroIndex(2)}),char(signalName{gyroIndex(3)})) 
                 
                 % Plot magnetometer data
-                subplot(4,1,3)
+                subplot(2,2,3)
                 plot(magDataShimmer1);                                                              
-                axis([0 NO_SAMPLES_IN_PLOT -2 2]);                           
+                axis([0 numPlotSamples -2 2]);
+                ylabel('Magnetic Flux Density (Ga)')
+                title([sprintf('Samp Rate: %4.1f',75) ' Hz'])
                 legend(char(signalName{magIndex(1)}),char(signalName{magIndex(2)}),char(signalName{magIndex(3)})) 
                 
                 % Plot ExG data
-                subplot(4,1,4)
-                plot(exgDataShimmer1);                                                             
-                axis([0 NO_SAMPLES_IN_PLOT -5 5]);                           
-                legend(char(signalName{exgIndex(1)}),char(signalName{exgIndex(2)})) 
+                subplot(2,2,4)
+                plot((emgDataShimmer1(:,1)-mean(emgDataShimmer1(:,1))/max(emgDataShimmer1(:,1))))
+                keyboard
+                hold on
+                plot(emgFiltDataShimmer1(:,1)/max(emgFiltDataShimmer1(:,1)));                                                             
+                axis([0 numPlotSamples -1 1]);
+                ylabel('Muscle Excitation (mV)')
+                title([sprintf('Samp Rate: %4.1f',1000) ' Hz'])
+                %legend(char(signalName{emgIndex(1)}),char(signalName{emgIndex(2)})) 
+                hold off
+                
+                figtitle(['Shimmer 1 Data - ' sprintf('Packets Received: %3.2f',packetsReceivedShimmer1) '%']);  
             end
-            elapsedTime = elapsedTime + toc; 
+            elapsedTime = elapsedTime + toc;
+            tic;
             
         end
-        elapsedTime = elapsedTime + toc;                                   % Stop timer
-        
-        shimmer1.stop;                                                     % Stop data streaming from shimmer1                                                    
-        shimmer2.stop;
+        elapsedTime = elapsedTime + toc;                                  
+        shimmer1.stop;  
         
     else
-        
-        shimmer1.stop;                                                     % Stop data streaming from shimmer1                                                    
-        shimmer2.stop;
-           
-    end
-    
-    shimmer1.disconnect;                                                   % Disconnect from shimmer1
-    shimmer2.disconnect;  
+        shimmer1.stop;                                                                                 
+    end    
+    shimmer1.disconnect;                                                
        
 end
-
 clear all;  
-
-
-
-
-
-
-
-
-
-
 
 end
