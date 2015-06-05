69c4b9777498c3ea6fd97203a984ba328d314264
nbianco3 <nick.a.bianco@gmail.com>
2015-06-05 14:24:21 -0400

    Version 1.2

--- NeuroDinnerPresentation.m
+++ NeuroDinnerPresentation.m
@@ -27,26 +27,94 @@ if (shimmer1.connect)
     
     if (shimmer1.start)
         
-        dataShimmer1 = [];
+        calDataShimmer1 = [];
         h.figure1=figure('Name','Shimmer 1');
         elapsedTime = 0;
         tic;
         
         while (elapsedTime < captureDuration)
+            
+            pause(DELAY)
+            packetRateShimmer1 = shimmer1.getpercentageofpacketsreceived();
         
+            % Read and plot data for shimmer1
+            [calData,signalName,signalFormat,signalUnit]=shimmer1.getdata('u');
+            keyboard
+            calDataShimmer1 = [calDataShimmer1; calData];    % Read the calibrated data for shimmer1 and add to previous data
             
+            if (length(calDataShimmer1)>numPlotSamples)
+                calDataShimmer1 = calDataShimmer1((length(calDataShimmer1)-numPlotSamples):end,:);
+            end
             
-            
+            if ~isempty(calDataShimmer1)
+                timeIndex = find(ismember(signalName, 'Time Stamp'));
+                accelIndex(1) = find(ismember(signalName, 'Low Noise Accelerometer X'));
+                accelIndex(2) = find(ismember(signalName, 'Low Noise Accelerometer Y'));
+                accelIndex(3) = find(ismember(signalName, 'Low Noise Accelerometer Z'));
+                gyroIndex(1) = find(ismember(signalName, 'Gyroscope X'));
+                gyroIndex(2) = find(ismember(signalName, 'Gyroscope Y'));
+                gyroIndex(3) = find(ismember(signalName, 'Gyroscope Z'));
+                magIndex(1) = find(ismember(signalName, 'Magnetometer X'));
+                magIndex(2) = find(ismember(signalName, 'Magnetometer Y'));
+                magIndex(3) = find(ismember(signalName, 'Magnetometer Z'));
+                exgIndex(1) = find(ismember(signalName, 'EXG1 CH1'));
+                exgIndex(2) = find(ismember(signalName, 'EXG1 CH2'));
+                
+                timeDataShimmer = calDataShimmer1(:,timeIndex);
+                packetsReceivedShimmer1 = shimmer1.getpercentageofpacketsreceived(timeDataShimmer);
+                accelDataShimmer1 = [calDataShimmer1(:,accelIndex(1)), calDataShimmer1(:,accelIndex(2)), calDataShimmer1(:,accelIndex(3))];
+                gyroDataShimmer1 = [calDataShimmer1(:,gyroIndex(1)), calDataShimmer1(:,gyroIndex(2)), calDataShimmer1(:,gyroIndex(3))];
+                magDataShimmer1 = [calDataShimmer1(:,magIndex(1)), calDataShimmer1(:,magIndex(2)), calDataShimmer1(:,magIndex(3))];
+                exgDataShimmer1 = [calDataShimmer1(:,exgIndex(1)), calDataShimmer1(:,exgIndex(2))];
+                
+                set(0,'CurrentFigure',h.figure1);
+                
+                % Plot accelerometer data
+                subplot(4,1,1)
+                plot(accelDataShimmer1);                                       
+                title(['Shimmer 1 Data - ' sprintf('Packets Received: %3.2f %',packetsReceivedShimmer1)]);                         
+                axis([0 NO_SAMPLES_IN_PLOT -20 20]);                           
+                legend(char(signalName{accelIndex(1)}),char(signalName{accelIndex(2)}),char(signalName{accelIndex(3)}))     
+                
+                % Plot gyroscope data
+                subplot(4,1,2)
+                plot(gyroDataShimmer1);                                                             
+                axis([0 NO_SAMPLES_IN_PLOT -500 500]);                           
+                legend(char(signalName{gyroIndex(1)}),char(signalName{gyroIndex(2)}),char(signalName{gyroIndex(3)})) 
+                
+                % Plot magnetometer data
+                subplot(4,1,3)
+                plot(magDataShimmer1);                                                              
+                axis([0 NO_SAMPLES_IN_PLOT -2 2]);                           
+                legend(char(signalName{magIndex(1)}),char(signalName{magIndex(2)}),char(signalName{magIndex(3)})) 
+                
+                % Plot ExG data
+                subplot(4,1,4)
+                plot(exgDataShimmer1);                                                             
+                axis([0 NO_SAMPLES_IN_PLOT -5 5]);                           
+                legend(char(signalName{exgIndex(1)}),char(signalName{exgIndex(2)})) 
+            end
+            elapsedTime = elapsedTime + toc; 
             
         end
+        elapsedTime = elapsedTime + toc;                                   % Stop timer
         
+        shimmer1.stop;                                                     % Stop data streaming from shimmer1                                                    
+        shimmer2.stop;
         
+    else
+        
+        shimmer1.stop;                                                     % Stop data streaming from shimmer1                                                    
+        shimmer2.stop;
+           
     end
     
-    
-    
-    
+    shimmer1.disconnect;                                                   % Disconnect from shimmer1
+    shimmer2.disconnect;  
+       
 end
+
+clear all;  
 
 
 
