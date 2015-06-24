e0d81a2ef012ee15fe62adad8e7ada2d7ab91ef6
nbianco3 <nick.a.bianco@gmail.com>
2015-06-24 08:55:26 -0400

    Matlab GUI Data Collection
    Update 1.6

--- SortShimmerDataAPI.m
+++ SortShimmerDataAPI.m
@@ -39,12 +39,12 @@ for i=1:numSensors
     end
 end
 
-handles = guihandles(gcbo);
-emg_h = handles.emgcheckbox;
+start_h = handles.startbutton;
+stop_h = handles.stopbutton;
+disconnect_h = handles.disconnectbutton;
 emgrate_h = handles.emgratemenu;
 emgresolution_h = handles.emgresolutionmenu;
 emggain_h = handles.emggainmenu;
-gsr_h = handles.gsrcheckbox;
 gsrrange_h = handles.gsrrangemenu;
 lownoise_h = handles.lownoiseaccelcheckbox;
 widerange_h = handles.widerangeaccelcheckbox;
@@ -59,40 +59,59 @@ samprate_h = handles.samprate;
 sampratebool_h = handles.sampratebool;
 pressure_h = handles.pressurecheckbox;
 pressureresolution_h = handles.pressuremenu;
+params_h = handles.setparams;
+elapsedtime_h = handles.elapsedtime;
+enable2BD1_h = handles.enable2BD1;
+enable3A45_h = handles.enable3A45;
+enable399C_h = handles.enable399C;
+enable3A1E_h = handles.enable3A1E;
+enable39F8_h = handles.enable39F8;
+enable2BFD_h = handles.enable2BFD;
+enable38F5_h = handles.enable38F5;
+emg2_h = handles.emg2;
+emg3_h = handles.emg3;
+emg4_h = handles.emg4;
+emg5_h = handles.emg5;
+imu1_h = handles.imu1;
+imu2_h = handles.imu2;
+imu3_h = handles.imu3;
+imu4_h = handles.imu4;
+imu5_h = handles.imu5;
+imu6_h = handles.imu6;
+imu7_h = handles.imu7;
+gsr_h = handles.gsrcheckbox;
+
+emgFlag = [0 get(emg2_h,'Value') get(emg3_h,'Value') get(emg4_h,'Value') get(emg5_h,'Value') 0 0];
+imuFlag = [get(imu1_h,'Value') get(imu2_h,'Value') get(imu3_h,'Value') get(imu4_h,'Value') get(imu5_h,'Value') get(imu6_h,'Value') get(imu7_h,'Value')];
 
 % Save parameters
 contents = cellstr(get(baudrate_h,'String'));
 trial.params.baudrate = [contents{get(baudrate_h,'Value')} 'kB/s'];
-if get(widerange_h,'Value')
-    contents = cellstr(get(accelrange_h,'String'));
-    trial.params.WRaccelrange = ['±' contents{get(accelrange_h,'Value')} 'g'];
-end
-contents = cellstr(get(gyrorange_h,'String'));
-trial.params.gyrorange = ['±' contents{get(gyrorange_h,'Value')} '°/s'];
-contents = cellstr(get(magrange_h,'String'));
-trial.params.magrange = ['±' contents{get(magrange_h,'Value')} 'Ga'];
+contents = cellstr(get(samprate_h,'String'));
+trial.params.generalsamplingrate = [contents{get(samprate_h,'Value')} 'Hz'];
 
-if ~get(sampratebool_h,'Value')
+if any(imuFlag)
     if get(widerange_h,'Value')
+        contents = cellstr(get(accelrange_h,'String'));
+        trial.params.WRaccelrange = ['±' contents{get(accelrange_h,'Value')} 'g'];
         contents = cellstr(get(accelrate_h,'String'));
         trial.params.WRaccelrate = [contents{get(accelrate_h,'Value')} 'Hz'];
     end
+    contents = cellstr(get(gyrorange_h,'String'));
+    trial.params.gyrorange = ['±' contents{get(gyrorange_h,'Value')} '°/s'];
+    contents = cellstr(get(magrange_h,'String'));
+    trial.params.magrange = ['±' contents{get(magrange_h,'Value')} 'Ga'];
     contents = cellstr(get(gyrorate_h,'String'));
     trial.params.gyrorate = [contents{get(gyrorate_h,'Value')} 'Hz'];
     contents = cellstr(get(magrate_h,'String'));
-    trial.params.magrate = [contents{get(magrate_h,'Value')} 'Hz'];
-    if get(emg_h,'Value')
-        contents = cellstr(get(emgrate_h,'String'));
-        trial.params.emgrate = [contents{get(emgrate_h,'Value')} 'Hz'];
-    end
-else
-    contents = cellstr(get(samprate_h,'String'));
-    trial.params.generalsamplingrate = [contents{get(samprate_h,'Value')} 'Hz'];
+    trial.params.magrate = [contents{get(magrate_h,'Value')} 'Hz']; 
 end
 
-if get(emg_h,'Value')
-    trial.params.emgresolution = get(emgresolution_h,'String');
-    trial.params.emggain = get(emggain_h,'String');
+if any(emgFlag)
+        contents = cellstr(get(emgrate_h,'String'));
+        trial.params.emgrate = [contents{get(emgrate_h,'Value')} 'Hz'];
+        trial.params.emgresolution = get(emgresolution_h,'String');
+        trial.params.emggain = get(emggain_h,'String');
 end
 
 if get(pressure_h,'Value')
